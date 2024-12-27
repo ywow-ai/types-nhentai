830f93b826c1a10bd320fc2f7bef956c64f33c33
ywow-ai <masyaqin37@gmail.com>
2024-12-27 09:14:42 +0700

    chore: remove index.d.ts and update package.json for module configuration

--- index.ts
+++ index.ts
@@ -1,36 +1,37 @@
-interface TitleT {
+export type TitleT = {
   english: string | null;
   japanese: string | null;
   pretty: string | null;
-}
+};
 
-interface ImageT {
+export type ImageT = {
   t: "j" | "p" | "w" | "g";
   w: number;
   h: number;
-}
+};
 
-interface ImagesT {
+export type ImagesT = {
   pages: ImageT[];
   cover: ImageT;
   thumbnail: ImageT;
-}
+};
 
-interface TagT {
+export type TagT = {
   id: number;
   type: string;
   name: string;
   url: string;
   count: number;
-}
+};
 
-interface BookT {
+export type BookT = {
   id: number;
   media_id: string;
+  title: TitleT;
   images: ImagesT;
   scanlator: string;
   upload_date: number;
   tags: TagT[];
   num_pages: number;
   num_favorites: number;
-}
+};
